ddf0e4b936adeaca9a63b6b099b747b49be7c32e
Samson Goodenough <goodenough.samson@gmail.com>
2021-05-15 00:21:22 -0400

    Test build

--- index.js
+++ index.js
@@ -86,5 +86,5 @@ app.on('activate', async () => {
 	mainWindow = await createMainWindow();
 
 	const favoriteAnimal = config.get('favoriteAnimal');
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
+	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'My favorite animal is ${favoriteAnimal}'`);
 })();
